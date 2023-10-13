f629865229bfe2ade9e6c564b5c38c697b163daf
Jason Dubar <buymeapc@gmail.com>
2023-10-12 20:56:54 -0400

    Add project files.

---
+++ HangTab/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HangTab
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ HangTab/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HangTab
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
