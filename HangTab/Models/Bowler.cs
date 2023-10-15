afd5c34121296e194d6b82b8fa9184c06fb4142b
Jason Dubar <buymeapc@gmail.com>
2023-10-14 23:07:40 -0400

    Added bowler model, img and baseviewmodel

---
+++ HangTab/Models/Bowler.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+
+namespace HangTab.Models;
+
+[Table("bowler")]
+public class Bowler
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string ImageUrl {  get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string FullName => $"{FirstName} {LastName}";
+
+    public Bowler Clone => MemberwiseClone() as Bowler;
+
+    public (bool IsValid, string ErrorMessage) Validate()
+    {
+        if (string.IsNullOrWhiteSpace(FirstName))
+        {
+            return (false, $"{nameof(FirstName)} is required.");
+        }
+        else if (string.IsNullOrWhiteSpace(LastName))
+        {
+            return (false, $"{nameof(LastName)} is required.");
+        }
+        return (true, string.Empty);
+    }
+}
---
+++ HangTab/ViewModels/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace HangTab.ViewModels;
+public partial class BaseViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private bool _isBusy;
+
+    [ObservableProperty]
+    private string _busyText;
+
+    public async Task ExecuteAsync(Func<Task> operation, string? busyText = null)
+    {
+        IsBusy = true;
+        BusyText = busyText ?? "Processing...";
+        try
+        {
+            await operation?.Invoke();
+        }
+        finally
+        {
+            IsBusy = false;
+            BusyText = "Processing...";
+        }
+    }
+
+}
