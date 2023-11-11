b0caf81aa8c57b9209dcae69809ace6886ed94ea
jdubar <55170139+jdubar@users.noreply.github.com>
2023-11-11 15:04:07 -0400

    Created bowlerimage component

---
+++ HangTab/Views/Components/BowlerImage.xaml.cs
@@ -0,0 +1,26 @@
+namespace HangTab.Views.Components;
+
+public partial class BowlerImage : ContentView
+{
+    public static readonly BindableProperty ImageUrlProperty =
+        BindableProperty.Create(nameof(ImageUrl), typeof(string), typeof(CardView), string.Empty);
+    public static readonly BindableProperty IsSubProperty =
+        BindableProperty.Create(nameof(IsSub), typeof(bool), typeof(CardView), false);
+
+    public string ImageUrl
+    {
+        get => (string)GetValue(ImageUrlProperty);
+        set => SetValue(ImageUrlProperty, value);
+    }
+
+    public bool IsSub
+    {
+        get => (bool)GetValue(IsSubProperty);
+        set => SetValue(IsSubProperty, value);
+    }
+
+    public BowlerImage()
+	{
+		InitializeComponent();
+	}
+}
