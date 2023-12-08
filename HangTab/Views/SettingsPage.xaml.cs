535bbc596db1735a7df2c990dd3abc463255dac2
jdubar <55170139+jdubar@users.noreply.github.com>
2023-12-08 15:01:12 -0500

    Initial Settings page items

--- HangTab/Views/SettingsPage.xaml.cs
+++ HangTab/Views/SettingsPage.xaml.cs
@@ -1,9 +1,19 @@
+using HangTab.ViewModels;
+
 namespace HangTab.Views;
 
 public partial class SettingsPage : ContentPage
 {
-	public SettingsPage()
+    private readonly BowlerViewModel _viewModel;
+
+    public SettingsPage(BowlerViewModel viewModel)
 	{
 		InitializeComponent();
+        _viewModel = viewModel;
 	}
+
+    private void ClearAllDataOnTap(object sender, TappedEventArgs e)
+    {
+        _viewModel.DropAllTablesCommand.ExecuteAsync(null);
+    }
 }
