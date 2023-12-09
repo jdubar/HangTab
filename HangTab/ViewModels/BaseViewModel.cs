1f07af51e2db90954d299998747eabd826018134
jdubar <55170139+jdubar@users.noreply.github.com>
2023-12-08 22:56:18 -0500

    Cleanup

--- HangTab/ViewModels/BaseViewModel.cs
+++ HangTab/ViewModels/BaseViewModel.cs
@@ -9,13 +9,13 @@ public partial class BaseViewModel : ObservableObject
     [ObservableProperty]
     private string _busyText;
 
-    public async Task ExecuteAsync(Func<Task> operation, string? busyText = null)
+    public async Task ExecuteAsync(Func<Task> operation, string busyText = null)
     {
         IsBusy = true;
         BusyText = busyText ?? "Processing...";
         try
         {
-            await operation?.Invoke();
+            await operation.Invoke();
         }
         finally
         {
