90ff8a283142880a09d88e124dfd582227fe6d4a
jdubar <55170139+jdubar@users.noreply.github.com>
2023-12-14 13:46:50 -0500

    Changed to file-based namespace

--- HangTab/App.xaml.cs
+++ HangTab/App.xaml.cs
@@ -1,12 +1,11 @@
-﻿namespace HangTab
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿namespace HangTab;
 
-            MainPage = new AppShell();
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
     }
 }
