cf70f4db19e51ce6046cd27c2f3b2af0e74fc45f
jdubar <55170139+jdubar@users.noreply.github.com>
2023-12-28 14:04:30 -0500

    Added crown

--- HangTab/Views/Components/BowlerImage.xaml.cs
+++ HangTab/Views/Components/BowlerImage.xaml.cs
@@ -4,6 +4,8 @@ public partial class BowlerImage : ContentView
 {
     public static readonly BindableProperty ImageUrlProperty =
         BindableProperty.Create(nameof(ImageUrl), typeof(string), typeof(CardView), string.Empty);
+    public static readonly BindableProperty IsLowestHangsProperty =
+        BindableProperty.Create(nameof(IsLowestHangs), typeof(bool), typeof(CardView), false);
     public static readonly BindableProperty IsSubProperty =
         BindableProperty.Create(nameof(IsSub), typeof(bool), typeof(CardView), false);
 
@@ -11,6 +13,12 @@ public partial class BowlerImage : ContentView
     {
         get => (string)GetValue(ImageUrlProperty);
         set => SetValue(ImageUrlProperty, value);
+    }
+
+    public bool IsLowestHangs
+    {
+        get => (bool)GetValue(IsLowestHangsProperty);
+        set => SetValue(IsLowestHangsProperty, value);
     }
 
     public bool IsSub
