4deb5119a96961756d8b53c4588e29fc3409753a
jdubar <55170139+jdubar@users.noreply.github.com>
2023-12-29 09:46:49 -0500

    Cleaned up platform code and changed ApplicationId

--- HangTab/Platforms/Android/MainApplication.cs
+++ HangTab/Platforms/Android/MainApplication.cs
@@ -1,16 +1,25 @@
 ﻿using Android.App;
 using Android.Runtime;
 
-namespace HangTab
-{
-    [Application]
-    public class MainApplication : MauiApplication
-    {
-        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-            : base(handle, ownership)
-        {
-        }
+// Needed for Picking photo/video
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage, MaxSdkVersion = 32)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaAudio)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaImages)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaVideo)]
 
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-    }
+// Needed for Taking photo/video
+[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage, MaxSdkVersion = 32)]
+
+// Filters out devices that do not have cameras (set to false to make them optional)
+[assembly: UsesFeature("android.hardware.camera", Required = true)]
+[assembly: UsesFeature("android.hardware.camera.autofocus", Required = true)]
+
+namespace HangTab;
+
+[Application]
+public class MainApplication(IntPtr handle, JniHandleOwnership ownership) : MauiApplication(handle, ownership)
+{
+    protected override MauiApp CreateMauiApp() =>
+        MauiProgram.CreateMauiApp();
 }
