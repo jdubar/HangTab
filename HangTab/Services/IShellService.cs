8da18117f10a33235b7dac821ca998fac68354b6
jdubar <55170139+jdubar@users.noreply.github.com>
2023-12-29 10:28:03 -0500

    Created ShellService

---
+++ HangTab/Services/IShellService.cs
@@ -0,0 +1,8 @@
+﻿namespace HangTab.Services;
+public interface IShellService
+{
+    Task DisplayAlert(string title, string msg, string buttonText);
+    Task<bool> DisplayPrompt(string title, string msg, string accept, string cancel);
+    Task GoToPage(ShellNavigationState state, bool animate);
+    Task ReturnToPage();
+}
---
+++ HangTab/Services/Impl/ShellService.cs
@@ -0,0 +1,15 @@
+﻿namespace HangTab.Services.Impl;
+public class ShellService : IShellService
+{
+    public async Task DisplayAlert(string title, string msg, string buttonText) =>
+        await Shell.Current.DisplayAlert(title, msg, buttonText);
+
+    public async Task<bool> DisplayPrompt(string title, string msg, string accept, string cancel) =>
+        await Shell.Current.DisplayAlert(title, msg, accept, cancel);
+
+    public async Task ReturnToPage() =>
+        await Shell.Current.GoToAsync("..", true);
+
+    public async Task GoToPage(ShellNavigationState state, bool animate) =>
+        await Shell.Current.GoToAsync(state, animate);
+}
