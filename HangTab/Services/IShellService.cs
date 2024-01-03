cbce833fd0d17e00bf4a78eff6e979049f74bed4
jdubar <55170139+jdubar@users.noreply.github.com>
2024-01-03 13:56:27 -0500

    Added toast method to ShellService

--- HangTab/Services/IShellService.cs
+++ HangTab/Services/IShellService.cs
@@ -5,6 +5,7 @@ public interface IShellService
 {
     Task DisplayAlert(string title, string msg, string buttonText);
     Task<bool> DisplayPrompt(string title, string msg, string accept, string cancel);
+    Task DisplayToast(string text);
     Task GoToPageWithData(ShellNavigationState state, Bowler bowler);
     Task ReturnToPage();
 }
--- HangTab/Services/Impl/ShellService.cs
+++ HangTab/Services/Impl/ShellService.cs
@@ -1,4 +1,7 @@
-﻿using HangTab.Models;
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+using HangTab.Models;
 
 namespace HangTab.Services.Impl;
 public class ShellService : IShellService
@@ -8,6 +11,17 @@ public class ShellService : IShellService
 
     public async Task<bool> DisplayPrompt(string title, string msg, string accept, string cancel) =>
         await Shell.Current.DisplayAlert(title, msg, accept, cancel);
+
+    public async Task DisplayToast(string text)
+    {
+        using var token = new CancellationTokenSource();
+        var duration = ToastDuration.Short;
+        var fontSize = 14;
+
+        var toast = Toast.Make(text, duration, fontSize);
+
+        await toast.Show(token.Token);
+    }
 
     public async Task GoToPageWithData(ShellNavigationState state, Bowler bowler)
     {
