dc29bf2771ca7730ef9d9b9174416f132c6845d5
jdubar <55170139+jdubar@users.noreply.github.com>
2024-01-05 11:01:45 -0500

    Created WeekViewModel

--- HangTab/Models/Week.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HangTab.Models;
-public class Week
-{
-    public int WeekNumber { get; set; }
-    public int TotalHangings { get; set; }
-    public int TotalBusRides { get; set; }
-}
---
+++ HangTab/ViewModels/WeekViewModel.cs
@@ -0,0 +1,10 @@
+﻿using HangTab.Models;
+
+namespace HangTab.ViewModels;
+public class WeekViewModel
+{
+    public int WeekNumber { get; set; }
+    public int TotalHangings { get; set; }
+    public int TotalBusRides { get; set; }
+    public IEnumerable<Bowler> Bowlers { get; set; }
+}
