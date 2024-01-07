cc7bba8363e37f846c65fe779391ebbcb214e4d1
jdubar <55170139+jdubar@users.noreply.github.com>
2024-01-07 07:28:51 -0500

    Made BusRides observable

--- HangTab/Models/Week.cs
+++ HangTab/Models/Week.cs
@@ -1,16 +1,23 @@
-﻿using SQLite;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+using SQLite;
 
 namespace HangTab.Models;
 
 [Table("week")]
-public class Week
+public class Week : ObservableObject
 {
+    private int _busRides;
+
     [PrimaryKey]
     public int Id { get; set; }
     public int WeekNumber { get; set; } = 1;
     public IEnumerable<Bowler> Bowlers { get; set; }
-    public int BusRides { get; set; }
-    public int BowlerIdWithLowestHangs { get; set; }
+    public int BusRides
+    {
+        get => _busRides;
+        set => SetProperty(ref _busRides, value);
+    }
 
     public int GetBowlerIdWithLowestHangs() =>
         Bowlers.First(b => !b.IsSub && b.TotalHangings == Bowlers.Where(b => !b.IsSub).Min(b => b.TotalHangings)).Id;
