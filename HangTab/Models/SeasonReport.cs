e7697df4b73c159ae3bb2535071f35a2a2318d70
jdubar <55170139+jdubar@users.noreply.github.com>
2024-01-10 15:40:42 -0500

    No longer needed

--- HangTab/Models/SeasonReport.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HangTab.Models;
-public class SeasonReport
-{
-    public IEnumerable<Bowler> AllBowlers { get; set; }
-    public IEnumerable<BowlerWeek> AllBowlerWeeks { get; set; }
-    public int LastSavedWeek { get; set; }
-}
