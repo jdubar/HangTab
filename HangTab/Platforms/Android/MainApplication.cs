ade674bb41d5e2035773b74ce588008a5755d885
jdubar <55170139+jdubar@users.noreply.github.com>
2024-01-11 11:07:33 -0500

    Updated Android manifest

--- HangTab/Platforms/Android/MainApplication.cs
+++ HangTab/Platforms/Android/MainApplication.cs
@@ -1,20 +1,6 @@
 ﻿using Android.App;
 using Android.Runtime;
 
-// Needed for Picking photo/video
-[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage, MaxSdkVersion = 32)]
-[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaAudio)]
-[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaImages)]
-[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaVideo)]
-
-// Needed for Taking photo/video
-[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage, MaxSdkVersion = 32)]
-
-// Filters out devices that do not have cameras (set to false to make them optional)
-[assembly: UsesFeature("android.hardware.camera", Required = true)]
-[assembly: UsesFeature("android.hardware.camera.autofocus", Required = true)]
-
 namespace HangTab;
 
 [Application]
