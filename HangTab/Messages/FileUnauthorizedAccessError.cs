b5239565b0ddcf8eb710043a016307f686fbfc2b
jdubar <55170139+jdubar@users.noreply.github.com>
2024-08-25 00:29:52 -0400

    Added Result Error messages

---
+++ HangTab/Messages/FileUnauthorizedAccessError.cs
@@ -0,0 +1,8 @@
+﻿namespace HangTab.Messages;
+
+public class FileUnauthorizedAccessError : Error
+{
+    public FileUnauthorizedAccessError(Exception err, string filePath)
+        : base($"Unauthorized access to path: '{filePath}'")
+    => _ = CausedBy(err);
+}
---
+++ HangTab/Messages/PathTooLongError.cs
@@ -0,0 +1,8 @@
+﻿namespace HangTab.Messages;
+
+public class PathTooLongError : Error
+{
+    public PathTooLongError(Exception err, string filePath)
+        : base($"Path is too long: '{filePath}'")
+        => _ = CausedBy(err);
+}
