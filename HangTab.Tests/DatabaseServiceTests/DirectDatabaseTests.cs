9905bb9be5b10ba928070c31611ce94732ac5e45
jdubar <55170139+jdubar@users.noreply.github.com>
2024-09-13 18:09:37 -0400

    More tests

--- HangTab.Tests/DatabaseServiceTests/DirectDatabaseTests.cs
+++ HangTab.Tests/DatabaseServiceTests/DirectDatabaseTests.cs
@@ -1,21 +1,60 @@
 ﻿using HangTab.Models;
+using HangTab.Tests.TestData;
 
 namespace HangTab.Tests.DatabaseServiceTests;
 public class DirectDatabaseTests : TestBase
 {
-    [Fact]
-    public async Task ItShouldDeleteAllTables()
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task ItShouldAttemptToDeleteAllTables(bool expected)
     {
         // Given
-        A.CallTo(() => ContextFake.DropTableAsync<Bowler>()).Returns(true);
-        A.CallTo(() => ContextFake.DropTableAsync<BowlerWeek>()).Returns(true);
-        A.CallTo(() => ContextFake.DropTableAsync<BusRide>()).Returns(true);
-        A.CallTo(() => ContextFake.DropTableAsync<BusRideWeek>()).Returns(true);
+        A.CallTo(() => ContextFake.DropTableAsync<Bowler>()).Returns(expected);
+        A.CallTo(() => ContextFake.DropTableAsync<BowlerWeek>()).Returns(expected);
+        A.CallTo(() => ContextFake.DropTableAsync<BusRide>()).Returns(expected);
+        A.CallTo(() => ContextFake.DropTableAsync<BusRideWeek>()).Returns(expected);
 
         // When
         var actual = await DatabaseService.DropAllTables();
 
         // Then
-        actual.Should().BeTrue();
+        actual.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task ItShouldAttemptToResetAllBowlerHangings(bool expected)
+    {
+        // Given
+        A.CallTo(() => ContextFake.GetAllAsync<Bowler>()).Returns(SimpleData.ListOfFiveBowlers);
+        A.CallTo(() => ContextFake.UpdateItemAsync(A<Bowler>.Ignored)).Returns(expected);
+        A.CallTo(() => ContextFake.DropTableAsync<BowlerWeek>()).Returns(true);
+        A.CallTo(() => ContextFake.DropTableAsync<BusRide>()).Returns(true);
+        A.CallTo(() => ContextFake.DropTableAsync<BusRideWeek>()).Returns(true);
+
+        // When
+        var actual = await DatabaseService.ResetHangings();
+
+        // Then
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public async Task ItShouldThrowOnResetAllBowlerHangings()
+    {
+        // Given
+        A.CallTo(() => ContextFake.GetAllAsync<Bowler>()).Returns(SimpleData.ListOfFiveBowlers);
+        A.CallTo(() => ContextFake.UpdateItemAsync(A<Bowler>.Ignored)).Throws<Exception>().Once().Then.Returns(false);
+        //A.CallTo(() => ContextFake.DropTableAsync<BowlerWeek>()).Returns(true);
+        //A.CallTo(() => ContextFake.DropTableAsync<BusRide>()).Returns(true);
+        //A.CallTo(() => ContextFake.DropTableAsync<BusRideWeek>()).Returns(true);
+
+        // When
+        var actual = await DatabaseService.ResetHangings();
+
+        // Then
+        actual.Should().BeFalse();
     }
 }
