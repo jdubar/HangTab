fe56c22122e897e45d20ddde69f0a06e324dac00
jdubar <55170139+jdubar@users.noreply.github.com>
2024-09-14 21:42:18 -0400

    Cleanup

--- HangTab.Tests/TestData/ListData.cs
+++ HangTab.Tests/TestData/ListData.cs
@@ -47,5 +47,4 @@ public class ListData
             IsLowestHangs = false,
         },
     ];
-
 }
--- HangTab.Tests/TestData/SimpleData.cs
+++ HangTab.Tests/TestData/SimpleData.cs
@@ -9,5 +9,4 @@ public static class SimpleData
     public static BusRide BusRide { get; } = new() { Id = 1, Total = 3 };
     public static BowlerWeek BowlerWeek { get; } = new() { Id = 2, BowlerId = 1, Hangings = 3, WeekNumber  = 2 };
     public static BusRideWeek BusRideWeek { get; } = new() { Id = 2, BusRides = 2, WeekNumber = 2 };
-
 }
