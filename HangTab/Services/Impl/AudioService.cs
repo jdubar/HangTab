536ce2f8a8ff0e3da555eede44ab169539fe95b8
jdubar <55170139+jdubar@users.noreply.github.com>
2025-03-21 10:11:40 -0400

    Fix namespace reference and clean up constants
    Updated `AudioService.cs` to reference `Constants` from the `Shared` namespace for correct access to `BusRideSoundFileName`. Removed the unused `DatabaseName` constant from `Constants.cs`.

--- HangTab/Services/Impl/AudioService.cs
+++ HangTab/Services/Impl/AudioService.cs
@@ -1,13 +1,11 @@
-﻿using HangTab.Shared;
-
-using Plugin.Maui.Audio;
+﻿using Plugin.Maui.Audio;
 
 namespace HangTab.Services.Impl;
 public class AudioService(IAudioManager audioManager) : IAudioService
 {
     public void PlayBusRideSound()
     {
-        using var stream = FileSystem.OpenAppPackageFileAsync(Constants.BusRideSoundFileName).Result;
+        using var stream = FileSystem.OpenAppPackageFileAsync(Shared.Constants.BusRideSoundFileName).Result;
         var player = audioManager.CreatePlayer(stream);
         player.Play();
     }
--- HangTab/Shared/Constants.cs
+++ HangTab/Shared/Constants.cs
@@ -1,8 +1,5 @@
 ﻿namespace HangTab.Shared;
 public static class Constants
 {
-    public static string DatabaseName => "Bowling.db3";
-
-    // ReSharper disable once StringLiteralTypo
     public static string BusRideSoundFileName => "beepbeep.mp3";
 }
