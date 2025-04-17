66eda94c2d7779ccffcb4bd4e6182c78502efb3b
jdubar <55170139+jdubar@users.noreply.github.com>
2025-04-17 10:14:52 -0400

    removed unused model

--- HangTab/Models/BowlerGroup.cs
+++
@@ -1,8 +0,0 @@
-﻿using HangTab.ViewModels;
-
-namespace HangTab.Models;
-
-public partial class BowlerGroup(string name, List<BowlerListItemViewModel> bowlers) : List<BowlerListItemViewModel>(bowlers)
-{
-    public string GroupName { get; private set; } = name;
-}
