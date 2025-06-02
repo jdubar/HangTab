791d328a8e22641d9064669b18f451d3c3ab0ed8
jdubar <55170139+jdubar@users.noreply.github.com>
2025-06-02 13:59:25 -0400

    Removed additional platforms from the project since it will be Android/Windows only now

--- HangTab/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace HangTab;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
