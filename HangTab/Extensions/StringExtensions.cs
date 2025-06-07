61098fb6e327fd7ca6f9d4a6fdb49c31053e8456
jdubar <55170139+jdubar@users.noreply.github.com>
2025-06-07 09:59:09 -0400

    made StringExtensions public for unit testing

--- HangTab/Extensions/StringExtensions.cs
+++ HangTab/Extensions/StringExtensions.cs
@@ -1,9 +1,9 @@
 ﻿using System.Text;
 
 namespace HangTab.Extensions;
-internal static class StringExtensions
+public static class StringExtensions
 {
-    internal static string GetInitials(this string name)
+    public static string GetInitials(this string name)
     {
         var nameSplit = name.Split([",", " "], StringSplitOptions.RemoveEmptyEntries);
         var stringBuilder = new StringBuilder();
