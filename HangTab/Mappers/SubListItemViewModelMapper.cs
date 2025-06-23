75c7196864a2b7f676b37e3b3fe209415482cd1e
jdubar <55170139+jdubar@users.noreply.github.com>
2025-06-23 11:12:08 -0400

    formatting fix

--- HangTab/Mappers/SubListItemViewModelMapper.cs
+++ HangTab/Mappers/SubListItemViewModelMapper.cs
@@ -4,7 +4,7 @@ using HangTab.ViewModels.Items;
 namespace HangTab.Mappers;
 public class SubListItemViewModelMapper : IMapper<IEnumerable<Person>, IEnumerable<SubListItemViewModel>>
 {
-    public  IEnumerable<SubListItemViewModel> Map(IEnumerable<Person> people)
+    public IEnumerable<SubListItemViewModel> Map(IEnumerable<Person> people)
     {
         return people is null
             ? throw new ArgumentNullException(nameof(people))
