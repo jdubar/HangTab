7a5a5678337844dda29fafacc1ba71b802883e01
jdubar <55170139+jdubar@users.noreply.github.com>
2025-06-28 11:33:53 -0400

    excluding UI code behinds from tests

--- HangTab/Views/BowlerSelectSubPage.xaml.cs
+++ HangTab/Views/BowlerSelectSubPage.xaml.cs
@@ -2,7 +2,7 @@
 using HangTab.Views.Base;
 
 namespace HangTab.Views;
-
+[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage(Justification = "We will not test the view code behind. There's no logic to test.")]
 public partial class BowlerSelectSubPage : ContentPageBase
 {
     public BowlerSelectSubPage(BowlerSelectSubViewModel vm)
