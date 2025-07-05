ef92dcc99a26235604edc2ecca96440fe5aad2d8
jdubar <55170139+jdubar@users.noreply.github.com>
2025-07-05 15:11:36 -0400

    Created a confetti view

---
+++ HangTab/Views/Controls/ConfettiView/ConfettiDrawable.cs
@@ -0,0 +1,20 @@
+﻿namespace HangTab.Views.Controls.ConfettiView;
+public class ConfettiDrawable(List<ConfettiParticle> particles) : IDrawable
+{
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        foreach (var p in particles.Where(p => p.Color is not null))
+        {
+            canvas.SaveState();
+            // Move to center of the square
+            canvas.Translate(p.X + p.Size / 2, p.Y + p.Size / 2);
+            canvas.Rotate(p.Rotation);
+            // Flip horizontally using scaleX (simulate flipping)
+            canvas.Scale(p.Flip, 1);
+            canvas.Translate(-p.Size / 2, -p.Size / 2);
+            canvas.FillColor = p.Color;
+            canvas.FillRectangle(0, 0, p.Size, p.Size);
+            canvas.RestoreState();
+        }
+    }
+}
---
+++ HangTab/Views/Controls/ConfettiView/ConfettiParticle.cs
@@ -0,0 +1,14 @@
+namespace HangTab.Views.Controls.ConfettiView;
+public class ConfettiParticle
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float SpeedY { get; set; }
+    public float SpeedX { get; set; }
+    public float Size { get; set; }
+    public Color? Color { get; set; }
+    public float Rotation { get; set; }
+    public float RotationSpeed { get; set; }
+    public float Flip { get; set; } // -1 to 1, for flipping effect
+    public float FlipSpeed { get; set; }
+}
