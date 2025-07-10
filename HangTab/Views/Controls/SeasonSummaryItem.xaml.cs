163c374c94da5dcc3d61226f0f579ed0a9b82443
jdubar <55170139+jdubar@users.noreply.github.com>
2025-07-10 19:23:29 -0400

    Add SeasonSummaryItem control and update UI
    Introduced a new `SeasonSummaryItem` control in `SeasonSummaryItem.xaml` and its code-behind, enabling data binding for properties like `Title`, `ImageSource`, `WeekNumber`, and `Count`. Updated `PersonListOverviewPage.xaml` to refine the `SearchBar` binding. Revamped `SeasonSummaryPage.xaml` by removing old labels and borders, replacing them with instances of `SeasonSummaryItem` to display summary information for hangings and bus rides.

---
+++ HangTab/Views/Controls/SeasonSummaryItem.xaml.cs
@@ -0,0 +1,45 @@
+namespace HangTab.Views.Controls;
+
+public partial class SeasonSummaryItem : ContentView
+{
+	public SeasonSummaryItem()
+	{
+		InitializeComponent();
+	}
+
+	public string Title
+	{
+		get => (string)GetValue(TitleProperty);
+		set => SetValue(TitleProperty, value);
+    }
+
+	public string ImageSource
+	{
+		get => (string)GetValue(ImageSourceProperty);
+		set => SetValue(ImageSourceProperty, value);
+    }
+	
+	public int WeekNumber
+	{
+		get => (int)GetValue(WeekNumberProperty);
+		set => SetValue(WeekNumberProperty, value);
+    }
+	
+	public int Count
+	{
+		get => (int)GetValue(CountProperty);
+		set => SetValue(CountProperty, value);
+    }
+
+	public static readonly BindableProperty TitleProperty = 
+		BindableProperty.Create(nameof(Title), typeof(string), typeof(SeasonSummaryItem), string.Empty);
+
+	public static readonly BindableProperty ImageSourceProperty =
+		BindableProperty.Create(nameof(ImageSource), typeof(string), typeof(SeasonSummaryItem), string.Empty);
+
+	public static readonly BindableProperty WeekNumberProperty =
+		BindableProperty.Create(nameof(WeekNumber), typeof(int), typeof(SeasonSummaryItem), 0);
+
+	public static readonly BindableProperty CountProperty =
+		BindableProperty.Create(nameof(Count), typeof(int), typeof(SeasonSummaryItem), 0);
+}
