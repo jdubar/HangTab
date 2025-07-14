1f1f34198c8a6256c63af1cbb41797d5c814b4f9
jdubar <55170139+jdubar@users.noreply.github.com>
2025-07-14 10:56:40 -0400

    excluded coverage for code behind

--- HangTab/Views/SeasonSummaryPage.xaml.cs
+++ HangTab/Views/SeasonSummaryPage.xaml.cs
@@ -2,7 +2,7 @@ using HangTab.ViewModels;
 using HangTab.Views.Base;
 
 namespace HangTab.Views;
-
+[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage(Justification = "We will not test the view code behind. There's no logic to test.")]
 public partial class SeasonSummaryPage : ContentPageBase
 {
 	public SeasonSummaryPage(SeasonSummaryViewModel vm)
