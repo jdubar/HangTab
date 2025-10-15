a8afe736e209e49c97f53bfe9358c9f227c98141
jdubar <55170139+jdubar@users.noreply.github.com>
2025-10-15 12:23:19 -0400

    updated tests for new services

---
+++ HangTab.Tests/Services/ScreenshotServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using HangTab.Repositories;
+using HangTab.Services.Impl;
+
+namespace HangTab.Tests.Services;
+public class ScreenshotServiceTests
+{
+    [Fact]
+    public async Task TakeScreenshotAsync_Successfully_ReturnsFilePath()
+    {
+        // Arrange
+        var expected = "path/to/screenshot.png";
+        var screenshotRepo = A.Fake<IScreenshotRepository>();
+        var storageRepo = A.Fake<IStorageRepository>();
+        var service = new ScreenshotService(screenshotRepo, storageRepo);
+        A.CallTo(() => screenshotRepo.TakeScreenshotAsync()).Returns(A.Fake<IScreenshotResult>());
+        A.CallTo(() => storageRepo.SaveScreenshotAsync(A<IScreenshotResult>._)).Returns(expected);
+
+        // Act
+        var actual = await service.TakeScreenshotAsync();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task TakeScreenshotAsync_OnNull_ReturnsEmpty()
+    {
+        // Arrange
+        var expected = string.Empty;
+        var screenshotRepo = A.Fake<IScreenshotRepository>();
+        var storageRepo = A.Fake<IStorageRepository>();
+        var service = new ScreenshotService(screenshotRepo, storageRepo);
+        A.CallTo(() => screenshotRepo.TakeScreenshotAsync()).Returns((IScreenshotResult?)null);
+
+        // Act
+        var actual = await service.TakeScreenshotAsync();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
--- HangTab/Services/Impl/ShareService.cs
+++ HangTab/Services/Impl/ShareService.cs
@@ -1,6 +1,7 @@
 ﻿using HangTab.Repositories;
 
 namespace HangTab.Services.Impl;
+[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage(Justification = "There is no functionality here to test.")]
 public class ShareService(IShareRepository share) : IShareService
 {
     public async Task ShareFileAsync(string filePath) => await share.ShareFileAsync(filePath);
