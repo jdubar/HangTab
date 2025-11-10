2e0c796bb663c795e9200720e29c4abba15865d0
jdubar <55170139+jdubar@users.noreply.github.com>
2025-11-09 21:51:39 -0500

    removed concrete repos and replaced with a base repo which takes care of basic CRUD operations

---
+++ HangTab/Repositories/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace HangTab.Repositories;
+public interface IBaseRepository<T> where T : class, new()
+{
+    Task<bool> AddAsync(T item);
+    Task<bool> DeleteByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetFilteredAsync(System.Linq.Expressions.Expression<Func<T, bool>> predicate);
+    Task<bool> UpdateAsync(T item);
+}
---
+++ HangTab/Repositories/Impl/BaseRepository.cs
@@ -0,0 +1,37 @@
+﻿using HangTab.Data;
+
+namespace HangTab.Repositories.Impl;
+public partial class BaseRepository<T>(IDatabaseContext context) : IBaseRepository<T> where T : class, new()
+{
+    public Task<bool> AddAsync(T item)
+    {
+        ArgumentNullException.ThrowIfNull(item);
+        return context.AddItemAsync(item);
+    }
+
+    public Task<bool> DeleteByIdAsync(int id)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(id);
+        return context.DeleteItemByIdAsync<T>(id);
+    }
+
+    public Task<IEnumerable<T>> GetAllAsync() => context.GetAllWithChildrenAsync<T>();
+
+    public Task<T> GetByIdAsync(int id)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(id);
+        return context.GetItemByIdAsync<T>(id);
+    }
+
+    public Task<IEnumerable<T>> GetFilteredAsync(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        return context.GetFilteredAsync(predicate);
+    }
+
+    public Task<bool> UpdateAsync(T item)
+    {
+        ArgumentNullException.ThrowIfNull(item);
+        return context.UpdateItemAsync(item);
+    }
+}
